19b0e0cf7ed86a588265f458798932eb811f155d
renegade84 <renegad84@gmail.com>
2023-02-26 19:55:00 +0300

    feat: game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0
+        this.min = 0
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return Math.round(this.min + ((this.max - this.min) / 2))
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
